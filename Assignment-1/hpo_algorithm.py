ce827cd2b84d473395a42aaa8c0eae39545ff3e9
yashbijalwan15 <yashbijalwan15@gmail.com>
2025-05-02 01:48:57 +0200

    add init to hpo_algorithm

--- Assignment-1/hpo_algorithm.py
+++ Assignment-1/hpo_algorithm.py
@@ -2,11 +2,17 @@ from abc import abstractmethod
 from ConfigSpace import Configuration, ConfigurationSpace
 
 
-class HPOAlgorithm:    
+class HPOAlgorithm:
+    def __init__(self, cs: ConfigurationSpace, total_budget: int, min_budget: int, max_budget: int) -> None:
+        self.cs: ConfigurationSpace = cs
+        self.budget: int = total_budget
+        self.min_budget: int = min_budget
+        self.max_budget: int = max_budget
+
     @abstractmethod
     def ask(self) -> tuple[dict, float]:
         pass
     
     @abstractmethod
-    def tell(self, config, result) -> None:
+    def tell(self, config: Configuration, result: float, budget: int) -> None:
         pass
