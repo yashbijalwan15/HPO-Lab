6dc151401da2c827bc35f7e2650ad1ec1ef57edd
yashbijalwan15 <yashbijalwan15@gmail.com>
2025-05-05 02:04:50 +0200

    implement random search algorithm

--- Assignment-1/random_search.py
+++ Assignment-1/random_search.py
@@ -1,4 +1,4 @@
-from ConfigSpace import Configuration, ConfigurationSpace
+from ConfigSpace import ConfigurationSpace
 from hpo_algorithm import HPOAlgorithm
 
 
@@ -11,10 +11,19 @@ class RandomSearch(HPOAlgorithm):
         max_budget: int,
     ) -> None:
         super().__init__(cs, total_budget, min_budget, max_budget)
-        pass
+        
+        n_init = int((total_budget * min_budget) // max_budget)
+        self.configs = self.sample(n_init)
+        self.evals = []
+        self.idx = 0
     
     def ask(self) -> tuple[dict, float]:
-        pass
+        if len(self.evals) == len(self.configs):
+            print(f"Configs Run: {len(self.configs)}")
+            return (None, self.max_budget)
+        
+        self.idx += 1
+        return (self.configs[self.idx - 1], self.max_budget)
     
-    def tell(self, config: Configuration, result: float, budget: int) -> None:
-        pass
+    def tell(self, config: dict, result: float, budget: int) -> None:
+        self.evals.append(result)
